d1eb10555a00153bfea0ec4f2483835204539115
Mikiuvu <2112046660@qq.com>
2026-01-30 07:37:29 +0800

    vofa更新

--- modules/vofa/vofa.c
+++ modules/vofa/vofa.c
@@ -1,33 +1,114 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: Chenfu
- * @Date: 2022-12-05 12:39:07
- * @LastEditTime: 2022-12-05 14:15:53
+/**
+ * @file vofa.c
+ * @brief VOFA+调试模块 - 支持多绑定组
+ * @note 接收格式: #ch=value! 例如 #0=3.14! #1=-1.5!
+ * @note 上位机中VOFA+配置:
+ *       比如：命令0：#1=%f! 代表通道1,%f为控件值占位符
  */
+
 #include "vofa.h"
+#include "bsp_usart.h"
+#include <string.h>
+#include <stdlib.h>
 
-/*VOFA浮点协议*/
-void vofa_justfloat_output(float *data, uint8_t num , UART_HandleTypeDef *huart )
+/* ======================== 绑定组结构体 ======================== */
+typedef struct {
+    float **ptrs;
+    uint8_t start_ch;
+    uint8_t count;
+} VofaBindGroup_t;
+
+/* ======================== 静态变量 ======================== */
+static float vofa_tx_data[VOFA_MAX_CH];
+static uint8_t vofa_tx_buf[VOFA_MAX_CH * 4 + 4];
+
+static VofaBindGroup_t vofa_bind_groups[VOFA_MAX_BIND_GROUPS];
+static uint8_t vofa_bind_group_count = 0;
+static USARTInstance *vofa_uart_instance = NULL;
+
+static const uint8_t VOFA_TAIL[4] = {0x00, 0x00, 0x80, 0x7f};
+
+/* ======================== 解析函数 ======================== */
+static void VofaParseAndUpdate(uint8_t *raw_buf, uint16_t max_len)
 {
-    static uint8_t i = 0;
-    send_float temp[num];			//定义缓冲区数组
-    uint8_t send_data[4 * num + 4]; //定义通过串口传出去的数组，数量是所传数据的字节数加上4个字节的尾巴
-    for (i = 0; i < num; i++)
-    {
-        temp[i].float_t = data[i]; //将所传数据移到缓冲区数组
+    char *buf = (char *)raw_buf;
+    
+    for (uint16_t i = 0; i < max_len && buf[i] != '\0'; i++) {
+        if (buf[i] != '#') continue;
+        
+        uint16_t end = i + 1;
+        while (end < max_len && buf[end] != '!' && buf[end] != '\0') end++;
+        if (end >= max_len || buf[end] != '!') continue;
+        
+        char *p = &buf[i + 1];
+        int ch = atoi(p);
+        
+        while (p < &buf[end] && *p != '=') p++;
+        if (*p != '=') continue;
+        p++;
+        
+        float value = (float)atof(p);
+        
+        // 遍历所有绑定组，找到匹配的通道
+        for (uint8_t g = 0; g < vofa_bind_group_count; g++) {
+            VofaBindGroup_t *grp = &vofa_bind_groups[g];
+            int idx = ch - grp->start_ch;
+            if (idx >= 0 && idx < grp->count && grp->ptrs[idx] != NULL) {
+                *grp->ptrs[idx] = value;
+                break;  // 一个通道只更新一次
+            }
+        }
+        
+        i = end;
     }
-    for (i = 0; i < num; i++)
-    {
-        send_data[4 * i] = temp[i].uint8_t[0];
-        send_data[4 * i + 1] = temp[i].uint8_t[1];
-        send_data[4 * i + 2] = temp[i].uint8_t[2];
-        send_data[4 * i + 3] = temp[i].uint8_t[3]; //将缓冲区数组内的浮点型数据转成4个字节的无符号整型，之后传到要通过串口传出的数组里
-    }
-    send_data[4 * num] = 0x00;
-    send_data[4 * num + 1] = 0x00;
-    send_data[4 * num + 2] = 0x80;
-    send_data[4 * num + 3] = 0x7f; //加上协议要求的4个尾巴
+}
 
-    HAL_UART_Transmit(huart, (uint8_t *)send_data, 4 * num + 4, 100);
+/* ======================== 回调函数 ======================== */
+static void VofaRxCallback(void)
+{
+    if (vofa_uart_instance == NULL) return;
+    VofaParseAndUpdate(vofa_uart_instance->recv_buff, 
+                       vofa_uart_instance->recv_buff_size);
+}
+
+/* ======================== 公开API ======================== */
+
+void VofaSend(UART_HandleTypeDef *uart, uint8_t start_ch, float *data, uint8_t len)
+{
+    if (uart == NULL) uart = &VOFA_UART;
+    if (uart->gState != HAL_UART_STATE_READY) return;
+    if (start_ch >= VOFA_MAX_CH || len == 0) return;
+    if (start_ch + len > VOFA_MAX_CH) len = VOFA_MAX_CH - start_ch;
+    
+    // 填充数据到对应通道
+    memcpy(&vofa_tx_data[start_ch], data, len * sizeof(float));
+    
+    // 打包发送
+    uint8_t max_ch = start_ch + len;
+    uint16_t tx_len = max_ch * sizeof(float);
+    memcpy(vofa_tx_buf, vofa_tx_data, tx_len);
+    memcpy(&vofa_tx_buf[tx_len], VOFA_TAIL, 4);
+    
+    HAL_UART_Transmit_DMA(uart, vofa_tx_buf, tx_len + 4);
+}
+
+void VofaBind(UART_HandleTypeDef *uart, uint8_t start_ch, float **ptrs, uint8_t len)
+{
+    if (vofa_bind_group_count >= VOFA_MAX_BIND_GROUPS) return;
+    
+    // 添加绑定组
+    VofaBindGroup_t *grp = &vofa_bind_groups[vofa_bind_group_count++];
+    grp->ptrs = ptrs;
+    grp->start_ch = start_ch;
+    grp->count = len;
+    
+    // 首次调用时注册串口
+    if (vofa_uart_instance == NULL) {
+        USART_Init_Config_s conf = {
+            .usart_handle = (uart != NULL) ? uart : &VOFA_UART,
+            .recv_buff_size = VOFA_MAX_CH * 4 + 4,
+            .module_callback = VofaRxCallback,
+        };
+        vofa_uart_instance = USARTRegister(&conf);
+    }
 }
--- modules/vofa/vofa.h
+++ modules/vofa/vofa.h
@@ -1,21 +1,62 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: Chenfu
- * @Date: 2022-12-05 12:39:18
- * @LastEditTime: 2022-12-05 13:37:36
+/**
+ * @file vofa.h
+ * @brief VOFA+调试模块
+ * 
+ * 【用法】
+ *   VOFA(0, yaw, pitch);           // 发送到通道0,1，上位机数据引擎选justfloat
+ *   VOFA(2, speed, accel);         // 发送到通道2,3
+ * 
+ *   VOFA_BIND(0, &kp, &ki);        // 绑定通道0,1
+ *   VOFA_BIND(2, &kd);             // 绑定通道2
+ * 
+ * 【指定串口】
+ *   VofaSend(&huart6, 0, data, 3); // 使用huart6发送
+ *   VofaBind(&huart6, 0, ptrs, 3); // 使用huart6接收
  */
+
 #ifndef VOFA_H
 #define VOFA_H
-#include <stdint.h>
-#include "bsp_usart.h"
+
 #include "usart.h"
+#include <stdint.h>
 
-typedef union
-{
-    float float_t;
-    uint8_t uint8_t[4];
-} send_float;
-void vofa_justfloat_output(float *data, uint8_t num , UART_HandleTypeDef *huart);
+/* ======================== 配置 ======================== */
+#define VOFA_UART           huart1      // 默认串口
+#define VOFA_MAX_CH         16          // 最大通道数
+#define VOFA_MAX_BIND_GROUPS 8          // 最大接收绑定组数，可根据需要调整
 
-#endif // !1#define 
+/**
+ * @brief 发送数据（指定起始通道）
+ */
+#define VOFA(start, ...) do { \
+    float _v[] = {__VA_ARGS__}; \
+    VofaSend(NULL, start, _v, sizeof(_v)/sizeof(float)); \
+} while(0)
+
+/**
+ * @brief 绑定接收变量（指定起始通道）
+ */
+#define VOFA_BIND(start, ...) do { \
+    static float *_p[] = {__VA_ARGS__}; \
+    VofaBind(NULL, start, _p, sizeof(_p)/sizeof(float*)); \
+} while(0)
+
+/**
+ * @brief 发送数据到VOFA
+ * @param uart 串口句柄，NULL则使用默认的VOFA_UART
+ * @param start_ch 起始通道号(0-15)
+ * @param data 数据数组
+ * @param len 数据个数
+ */
+void VofaSend(UART_HandleTypeDef *uart, uint8_t start_ch, float *data, uint8_t len);
+
+/**
+ * @brief 绑定接收变量
+ * @param uart 串口句柄，NULL则使用默认的VOFA_UART
+ * @param start_ch 起始通道号
+ * @param ptrs 变量指针数组
+ * @param len 变量个数
+ */
+void VofaBind(UART_HandleTypeDef *uart, uint8_t start_ch, float **ptrs, uint8_t len);
+
+#endif
